a50a446c045ff91730e6fd345d5457da5461ac23
Nguyễn Thái An <thaian229@gmail.com>
2020-10-06 09:32:22 +0700

    Init commit of Parser

--- Scanner/Scanner_Modified/token.c
+++ Scanner/Scanner_Modified/token.c
@@ -12,33 +12,34 @@ struct
 {
     char string[MAX_IDENT_LEN + 1];
     TokenType tokenType;
-} keywords[KEYWORDS_COUNT] = {
-    {"PROGRAM", KW_PROGRAM},
-    {"CONST", KW_CONST},
-    {"TYPE", KW_TYPE},
-    {"VAR", KW_VAR},
-    {"INTEGER", KW_INTEGER},
-    {"CHAR", KW_CHAR},
-    {"ARRAY", KW_ARRAY},
-    {"OF", KW_OF},
-    {"FUNCTION", KW_FUNCTION},
-    {"PROCEDURE", KW_PROCEDURE},
-    {"BEGIN", KW_BEGIN},
-    {"END", KW_END},
-    {"CALL", KW_CALL},
-    {"IF", KW_IF},
-    {"THEN", KW_THEN},
-    {"ELSE", KW_ELSE},
-    {"WHILE", KW_WHILE},
-    {"DO", KW_DO},
-    {"FOR", KW_FOR},
-    {"TO", KW_TO}};
+} keywords[KEYWORDS_COUNT] = {  
+    // Change all keyword to lowercase to sastifies new requirement
+    {"program", KW_PROGRAM},
+    {"const", KW_CONST},
+    {"type", KW_TYPE},
+    {"var", KW_VAR},
+    {"integer", KW_INTEGER},
+    {"char", KW_CHAR},
+    {"array", KW_ARRAY},
+    {"of", KW_OF},
+    {"function", KW_FUNCTION},
+    {"procedure", KW_PROCEDURE},
+    {"begin", KW_BEGIN},
+    {"end", KW_END},
+    {"call", KW_CALL},
+    {"if", KW_IF},
+    {"then", KW_THEN},
+    {"else", KW_ELSE},
+    {"while", KW_WHILE},
+    {"do", KW_DO},
+    {"for", KW_FOR},
+    {"to", KW_TO}};
 
 int keywordEq(char *kw, char *string)
 {
     while ((*kw != '\0') && (*string != '\0'))
     {
-        if (*kw != toupper(*string))
+        if (*kw != *string) // Now it's case sensitive
             break;
         kw++;
         string++;
